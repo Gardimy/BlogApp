663049a455ee155ac220d84a112039b9e6fc1b7d
Gardimy Charles <gardimycharles889@gmail.com>
2023-08-31 09:43:29 -0400

    Generate the migrations and models

--- db/migrate/20230831130253_create_likes.rb
+++ db/migrate/20230831130253_create_likes.rb
@@ -1,11 +1,10 @@
-class CreateComments < ActiveRecord::Migration[6.1]
+class CreateLikes < ActiveRecord::Migration[7.0]
   def change
-    create_table :comments do |t|
+    create_table :likes do |t|
       t.references :author, foreign_key: { to_table: :users }
       t.references :post, foreign_key: true
-      t.text :text
+
       t.timestamps
     end
-    add_index :comments, :post_id
   end
 end
