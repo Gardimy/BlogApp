5dd22ebbb82d356c4d4f59fc98584d0f45c277ae
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-06 09:26:09 -0400

    Update all controllers files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,18 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
+	@user = User.find(user_id)
+	@posts = @user.posts.includes(:comments)
   end
 
   def show
     @post = Post.find(params[:id])
+	@posts = @user.posts
+	@user = User.find(params[:user_id])
+	@user = User.find(user_id)
+	id = params[:id]
+    @posts = @user.posts
+    @targated_post = @posts.find(id)
+    @comments = @targated_post.comments
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user = User.find(user_id)
+    @posts = @user.posts
   end
 end
