03283aa021af0d869f7da7de3486cce7156b5d0d
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-06 09:49:22 -0400

    Fix rubocop issues!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,14 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
-	@user = User.find(user_id)
-	@posts = @user.posts.includes(:comments)
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
     @post = Post.find(params[:id])
-	@posts = @user.posts
-	@user = User.find(params[:user_id])
-	@user = User.find(user_id)
-	id = params[:id]
     @posts = @user.posts
+    @user = User.find(params[:user_id])
+    id = params[:id]
     @targated_post = @posts.find(id)
     @comments = @targated_post.comments
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @user = User.find(user_id)
     @posts = @user.posts
   end
 end
