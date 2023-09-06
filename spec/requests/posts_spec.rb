17791e2c923a5ff0dc96b1980351f49f98211581
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-06 16:36:39 -0400

    Update the views files

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "PostsController", type: :request do
-  describe "GET /posts" do
-    it "returns a successful response" do
-      get posts_path
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /users/:user_id/posts' do
+    let(:user) { User.create(name: 'Test User') }
+
+    it 'returns a successful response' do
+      get "/users/#{user.id}/posts"
       expect(response).to have_http_status(200)
     end
 
-    it "renders the index template" do
-      get posts_path
+    it 'renders the index template' do
+      get "/users/#{user.id}/posts"
       expect(response).to render_template(:index)
     end
 
-    it "includes correct placeholder text in the response body" do
-      get posts_path
-      expect(response.body).to include("Listing posts")
+    it 'includes correct placeholder text in the response body' do
+      get "/users/#{user.id}/posts"
+      expect(response.body).to include('List of all posts')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,20 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe "UsersController", type: :request do
-  describe "GET /users" do
-    it "returns a successful response" do
-      get users_path
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
+    it 'returns a successful response' do
+      get '/users'
       expect(response).to have_http_status(200)
     end
 
-    it "renders the index template" do
-      get users_path
+    it 'renders the index template' do
+      get '/users'
       expect(response).to render_template(:index)
     end
 
-    it "includes correct placeholder text in the response body" do
-      get users_path
-      expect(response.body).to include("Listing users")
+    it 'includes correct placeholder text in the response body' do
+      get '/users'
+      expect(response.body).to include('List of all users')
+    end
+  end
+
+  describe 'GET /users/:id' do
+    let(:user) { User.create(name: 'Test User') }
+
+    it 'returns a successful response' do
+      get "/users/#{user.id}"
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      get "/users/#{user.id}"
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get "/users/#{user.id}"
+      expect(response.body).to include('Number of Posts')
     end
   end
 end
