0d6aff571c5356632408ad8873054db8207df1e6
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-06 23:00:42 -0400

    Remove the pending file and add test for sow page

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,18 +5,38 @@ RSpec.describe 'Posts', type: :request do
     let(:user) { User.create(name: 'Test User') }
 
     it 'returns a successful response' do
-      get "/users/#{user.id}/posts"
+      get user_posts_path(user)
       expect(response).to have_http_status(200)
     end
 
     it 'renders the index template' do
-      get "/users/#{user.id}/posts"
+      get user_posts_path(user)
       expect(response).to render_template(:index)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get "/users/#{user.id}/posts"
+      get user_posts_path(user)
       expect(response.body).to include('List of all posts')
+    end
+  end
+
+  describe 'GET /posts/:id' do
+    let(:user) { User.create(name: 'Test User') }
+    let(:post) { Post.create(title: 'Test Post', author: user) }
+
+    it 'returns a successful response' do
+      get post_path(post)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show template' do
+      get post_path(post)
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the post title in the response body' do
+      get post_path(post)
+      expect(response.body).to include(post.title)
     end
   end
 end
