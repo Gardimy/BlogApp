18ecab329f37c09a4540638a843ccc0cdf21c9c9
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-09 19:35:38 -0400

    Fix linters and rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,17 +6,17 @@ class PostsController < ApplicationController
   end
 
   def show
-	@user = User.find(params[:user_id])
-	@targated_post = Post.find_by(id: params[:id])
-  
-	if @targated_post.nil?
-	  flash[:error] = "Post not found"
-	  redirect_to user_posts_path(@user)
-	else
-	  @comments = @targated_post.comments
-	end
+    @user = User.find(params[:user_id])
+    @targated_post = Post.find_by(id: params[:id])
+
+    if @targated_post.nil?
+      flash[:error] = 'Post not found'
+      redirect_to user_posts_path(@user)
+    else
+      @comments = @targated_post.comments
+    end
   end
-  
+
   private
 
   def find_user
