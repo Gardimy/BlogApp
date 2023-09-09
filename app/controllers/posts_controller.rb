3355c33a295a0f4ce6aa762581a6099c5c69a7c5
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-09 19:25:45 -0400

    Fix the fourth design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,17 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
-    @comments = @post.comments
+	@user = User.find(params[:user_id])
+	@targated_post = Post.find_by(id: params[:id])
+  
+	if @targated_post.nil?
+	  flash[:error] = "Post not found"
+	  redirect_to user_posts_path(@user)
+	else
+	  @comments = @targated_post.comments
+	end
   end
-
+  
   private
 
   def find_user
