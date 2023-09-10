7baecf37c41e7602b6152e54d1db260dffaec63c
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-09 21:30:42 -0400

    Create comment and like controllers files

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 	root "users#index"
     resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
+	resources :comments, only: [:create]
+    resources :likes, only: [:create]
   end
 
   resources :posts, only: [:show]
