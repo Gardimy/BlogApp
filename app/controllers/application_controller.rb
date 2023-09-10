5739ff78cb7ec6c24f3d649e699f627033a0de27
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-10 01:10:27 -0400

    Fix rubocop issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user
 
   def set_current_user
-    @_current_user ||= User.first
+    @current_user ||= User.first
   end
 end
