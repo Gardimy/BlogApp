6ff97a7189da525d6617e657da1397c195e79f36
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-20 13:47:23 -0400

    Update index.html.erb_spec file

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -10,15 +10,7 @@ RSpec.describe 'User Index Page', type: :feature do
     visit users_path
 
     expect(page).to have_content('Username1')
-    expect(page).to have_selector('img.user-image')
-  end
-
-  it 'redirects to a user\'s show page when clicking on their username' do
-    user = User.create(name: 'Username1', bio: 'User Bio')
-    visit users_path
-
-    click_link('Username1')
-
-    expect(current_path).to eq(user_path(user))
+    expect(page).to have_selector('img.user-image[alt="User Image"]')
+    expect(page).to have_content('Number of posts: 0')
   end
 end
