7758ccf34f5fe95e232bfd17216ff8f471e90d43
JuanDBta <simplebetsas@gmail.com>
2023-09-20 10:35:27 -0500

    Fix typo errors from show posts tests

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -2,14 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Post Show Page', type: :feature do
   before do
-    # Add setup code here if needed (e.g., create a post and comments)
   end
 
   it 'displays post details and comments' do
     visit post_path(Post.first)
 
-    # Add Capybara assertions to check for post details and comments
-    # expect(page).to have_content('Commenter2:')
-    # expect(page).to have_content('Commenter2\'s Comment')
   end
 end
