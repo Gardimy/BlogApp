1546b8741056147680447c6e515a5ce2e0b08a02
JuanDBta <simplebetsas@gmail.com>
2023-09-20 10:33:27 -0500

    Fixed users index test

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'User Index Page', type: :feature do
   before do
-    # Add setup code here if needed (e.g., create some user records)
   end
 
   it 'displays username, profile picture, and post count for a user' do
     User.create(name: 'Username1', bio: 'User Bio')
     visit users_path
 
-    # Add Capybara assertions to check for username, profile picture, and post count
     expect(page).to have_content('Username1')
     expect(page).to have_selector('img.user-image')
   end
@@ -18,7 +16,6 @@ RSpec.describe 'User Index Page', type: :feature do
     user = User.create(name: 'Username1', bio: 'User Bio')
     visit users_path
 
-    # Click on the user's username link
     click_link('Username1')
 
     expect(current_path).to eq(user_path(user))
