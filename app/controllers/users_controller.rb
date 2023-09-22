98dc00523cb47667dc4f4543346a0d53ed1bb307
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-21 23:45:46 -0400

    Set up devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-
   def index
     @users = User.all
   end
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
     @posts = @user.posts
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -48,7 +48,7 @@ Devise.setup do |config|
   # or not authentication should be aborted when the value is not present.
   # config.authentication_keys = [:email]
 
-#  config.authentication_keys = [:email, :name]
+  config.authentication_keys = [:email, :name]
 
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
