b98323886176c684c4a7fff9fc1e604a64c82821
Gardimy Charles <gardimycharles889@gmail.com>
2023-09-22 11:40:39 -0400

    Update the application_controller file and add confirmable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
-
-  def set_current_user
-    @set_current_user ||= User.first
-  end
+  # Remove the before_action and set_current_user method
+  # ...
 
   protected
 
