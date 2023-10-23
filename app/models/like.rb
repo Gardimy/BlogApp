eb7aba11ce2b4ea720762d0da1f6260a85f1c71a
Gardimy Charles <gardimycharles889@gmail.com>
2023-10-23 15:11:03 -0400

    Update the model file

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,12 @@
-class Like < ActiveRecord::Base
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :post
+#class Like < ActiveRecord::Base
+#  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+#  belongs_to :post
 
-  after_create :increment_like_counter
+#  after_create :increment_like_counter
 
-  private
+#  private
 
-  def increment_like_counter
-    post.increment(:likes_counter)
-  end
-end
+#  def increment_like_counter
+#    post.increment(:likes_counter)
+#  end
+#end
