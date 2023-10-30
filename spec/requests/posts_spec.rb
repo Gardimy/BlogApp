47e8a9eebadd5b8aa1d3ba3a6e179632c9273735
Gardimy Charles <gardimycharles889@gmail.com>
2023-10-30 14:32:20 -0400

    Update posts_spec.rb

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  describe 'GET /users/:user_id/posts' do
+    let(:user) { User.create(name: 'Test User') }
+
+    it 'returns a successful response' do
+      get user_posts_path(user)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      get user_posts_path(user)
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get user_posts_path(user)
+      expect(response.body).to include('List of all posts')
+    end
+  end
+
   describe 'GET /posts/:id' do
     let(:user) { User.create(name: 'Test User') }
     let(:post) { Post.create(title: 'Test Post', author: user) }
-    let(:current_user) { User.create(name: 'Current User') }
 
     it 'returns a successful response' do
       get post_path(post)
@@ -18,7 +36,7 @@ RSpec.describe 'Posts', type: :request do
 
     it 'includes the post title in the response body' do
       get post_path(post)
-      #  expect(response.body).to include(post.title)
+      expect(response.body).to include(post.title)
     end
   end
 end
