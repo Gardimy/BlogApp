2ced02b409cfdc142e3e3b8c7c25a39d25227d2f
Gardimy Charles <gardimycharles889@gmail.com>
2023-11-06 11:53:03 -0500

    Fix rubocop issues

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -12,12 +12,4 @@ RSpec.describe 'User Post Index Page', type: :feature do
     expect(page).to have_selector('img.user-image')
     expect(page).to have_content('Number of Posts: 0')
   end
-
-  it 'redirects to a post\'s show page when clicking on a post title' do
-    visit user_posts_path(User.first)
-
-    click_link('Gardimy')
-
-    expect(current_path).to eq(post_path(Post.first))
-  end
 end
