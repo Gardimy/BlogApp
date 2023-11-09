a746066ea326536fde6f93ecd34639fa6f1ba606
Gardimy Charles <gardimycharles889@gmail.com>
2023-11-09 15:32:56 -0500

    AAdd authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters
   before_action :load_ability
+  before_action :configure_permitted_parameters
+
 
   # ...
 
